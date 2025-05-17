c5afcf64f951258c355608ee8fb36f70e38cb4ce
Radostina Angelova <radostina.angelova@MUSALA.COM>
2025-05-17 16:57:33 +0300

    increment decrement and remove

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -28,11 +28,21 @@ const handleCheckoutShopping = (e) => {
   alert('Functionality to be added for future reference');
 };
 
-  const handleIncrement = (item) => {};
+  const handleIncrement = (item) => {
+  dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+};
 
-  const handleDecrement = (item) => {};
+  const handleDecrement = (item) => {
+  if (item.quantity > 1) {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+  } else {
+    dispatch(removeItem(item.name));
+  }
+};
 
-  const handleRemove = (item) => {};
+  const handleRemove = (item) => {
+  dispatch(removeItem(item.name));
+};
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
